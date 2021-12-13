a546e1d44f2011d69ad41248dce4b16fea5d53f5
ntzwilly <ntzwilly@gmail.com>
2021-12-13 16:58:39 +0200

    Create Student class

---
+++ student.rb
@@ -0,0 +1,10 @@
+module Student 
+    class Student < Person
+        def initialize(classroom)
+            @classroom = classroom
+        end
+        def play_hooky 
+            "¯\(ツ)/¯"
+        end
+    end
+end
