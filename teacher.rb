9b0ea0ec19a9aee62ab2868484d6c093d840d20f
ntzwilly <ntzwilly@gmail.com>
2021-12-13 16:59:23 +0200

    Create Teacher class

---
+++ teacher.rb
@@ -0,0 +1,10 @@
+module Teacher
+    class Teacher < Person
+        def initialize(specialization)
+            @specialization = specialization
+        end
+        def can_use_services? 
+            true
+        end
+    end    
+end
