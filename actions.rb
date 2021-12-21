6efeca63977b21dda0fbf780100400f23a0e3a5f
ntzwilly <ntzwilly@gmail.com>
2021-12-21 11:09:35 +0200

    Move list_people method into actions.rb

--- actions.rb
+++ actions.rb
@@ -9,4 +9,17 @@ module Actions
   def list_people
     @people.each { |person| puts person }
   end
+  def create_person
+    print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
+    choice = gets.chomp
+    case choice
+    when '1'
+      create_student
+    when '2'
+      create_teacher
+    else
+      puts 'That is not a valid input'
+      nil
+    end
+  end
 end
