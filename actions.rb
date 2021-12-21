1d529d3820859a479ee0e6e7e318254cf4201b17
HENRY-KC <henrykc24@yahoo.com>
2021-12-21 10:17:12 +0100

    Move create_book method to actions.rb

--- actions.rb
+++ actions.rb
@@ -49,4 +49,14 @@ module Actions
     @people.push(teacher)
     puts 'Person created successfully'
   end
+
+  def create_book
+    print 'Title: '
+    title = gets.chomp
+    print 'Author: '
+    author = gets.chomp
+    book = Book.new(title, author)
+    @books.push(book)
+    puts 'Book created successfully'
+  end
 end
