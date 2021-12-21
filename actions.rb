959e9ac655b726f5bed7d721f3937c9d450b51af
HENRY-KC <henrykc24@yahoo.com>
2021-12-21 10:08:05 +0100

    Move list_people method to actions.rb

--- actions.rb
+++ actions.rb
@@ -1,7 +1,12 @@
 require './book'
+require './person'
 
 module Actions
   def list_books
     @books.each { |book| puts book }
+  end
+
+  def list_people
+    @people.each { |person| puts person }
   end
 end
