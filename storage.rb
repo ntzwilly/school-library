06eb10ca99a8231d848a560c3adadde6dc255202
HENRY-KC <henrykc24@yahoo.com>
2021-12-23 12:58:07 +0100

    Add parse_rentals method in storage class

--- storage.rb
+++ storage.rb
@@ -22,4 +22,20 @@ class Storage
       []
     end
   end
+
+  def parse_rentals(people, books)
+    file = 'rentals.json'
+
+    if File.exist? file
+      JSON.parse(File.read(file)).map do |rental_json|
+        book = books.find { |current_book| current_book.title == rental_json['book_title'] }
+        person = people.find { |current_person| current_person.id == rental_json['person_id'].to_i }
+
+        Rental.new(rental_json['date'], book, person)
+      end
+    else
+      []
+    end
+  end
+
 end
