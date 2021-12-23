d3d452dc23590d72e540124c0b8bce2a9cf8052e
ntzwilly <ntzwilly@gmail.com>
2021-12-23 18:19:12 +0200

    Add tests for Teacher Class

--- person.rb
+++ person.rb
@@ -4,7 +4,7 @@ class Person
   attr_accessor :id, :name, :age
   attr_reader :parent_permission, :rentals
 
-  def initialize(age:, name: 'Unknow', parent_permission: true)
+  def initialize(age:, name: 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -14,7 +14,7 @@ class Person
   end
 
   def can_use_services?
-    is_of_age? || parent_permission
+    of_age? || parent_permission
   end
 
   def validate_name
