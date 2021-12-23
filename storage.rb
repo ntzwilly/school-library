21de11c885597c359cada1a0d549bf58b424c0e6
ntzwilly <ntzwilly@gmail.com>
2021-12-23 13:54:09 +0200

    Create method hydrate

--- storage.rb
+++ storage.rb
@@ -1,5 +1,15 @@
 require 'json'
 
 class Storage
-  
+  def hydrate(classroom)
+    books = parse_books
+    people = parse_people(classroom)
+    rentals = parse_rentals(people, books)
+
+    {
+      'people' => people,
+      'books' => books,
+      'rentals' => rentals
+    }
+  end
 end
