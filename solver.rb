0fc2031b4da928c092703dabcd1ef2b3d0fd1459
ntzwilly <ntzwilly@gmail.com>
2021-12-24 10:05:27 +0200

    Create fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -15,4 +15,16 @@ class Solver
     split_str.each { |str| reverse_str.unshift(str) }
     reverse_str.join
   end
+
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 5).zero?
+      'buzz'
+    elsif (num % 3).zero?
+      'fizz'
+    else
+      num.to_s
+    end
+  end
 end
