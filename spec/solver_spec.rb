17f4ee3ff1aeecbb38f9748eef96f98988f63ca2
HENRY-KC <henrykc24@yahoo.com>
2021-12-24 09:36:02 +0100

    Tests factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -13,5 +13,10 @@ describe Solver do
     it 'Checks the existence of the factorial method' do
       expect(@solve).to respond_to :factorial
     end
+
+    it 'Should check if factorial method works as' do
+      factorial_of_3 = @solve.factorial(3)
+      expect(factorial_of_3).to be 6
+    end
   end
 end
